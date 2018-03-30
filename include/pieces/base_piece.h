21973a9642064d00fffdc40c4e31870207e76cd5
Arthur Quinn <arthurquinn92@gmail.com>
2018-03-30 01:50:40 -0400

    added base piece class; added queen class

---
+++ include/pieces/base_piece.h
@@ -0,0 +1,15 @@
+#ifndef __BASE_PIECE_H
+#define __BASE_PIECE_H
+
+
+class BasePiece {
+private:
+
+
+public:
+    BasePiece() = default;
+    virtual ~BasePiece() = 0;
+};
+
+
+#endif
---
+++ include/pieces/queen.h
@@ -0,0 +1,18 @@
+#ifndef __QUEEN_H
+#define __QUEEN_H
+
+#include "pieces/base_piece.h"
+
+class Queen : public BasePiece {
+private:
+
+
+public:
+    Queen() = default;
+    virtual ~Queen() = default;
+
+};
+
+
+
+#endif
