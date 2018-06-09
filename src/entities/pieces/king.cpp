2c13a5cb798c2e646512ed0094ead44335cb8411
Arthur Quinn <arthurquinn92@gmail.com>
2018-06-08 20:53:22 -0400

    implement king possible moves

--- src/entities/pieces/king.cpp
+++ src/entities/pieces/king.cpp
@@ -4,5 +4,18 @@
 using Location = King::Location;
 
 std::vector<Location> King::possible_moves(const Board& board) const {
+    auto locs = std::vector<Location>();
+    const auto& d = { -1, 0, 1 };
+    for (const auto& dr : d) {
+        for (const auto& df : d) {
+            if (dr == 0 && df == 0) continue;
 
+            const auto& rank = _location.first + dr;
+            const auto& file = _location.second + df;
+            if (board.in_bounds(rank, file) && board.empty(rank, file)) {
+                locs.emplace_back(rank, file);
+            }
+        }
+    }
+    return locs;
 }
